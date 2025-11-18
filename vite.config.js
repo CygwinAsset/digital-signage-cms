9978f4d63e64eebf7e80845452b95fc7346a8ec3
siskalab <assetareajkt@gmail.com>
2025-11-18 18:48:29 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/digital-signage-cms/', // <-- TAMBAHKAN BARIS INI
   build: {
-    outDir: 'docs' // <-- INI YANG DIUBAH
+    outDir: 'docs'
   }
 })
